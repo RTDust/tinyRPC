b6d1d76461ccb6d44ca9bd7a9b7dfb23b6cb97dd
RTDust <chunj@hust.edu.cn>
2023-08-11 01:15:05 -0400

    1

---
+++ rocket/common/log.cc
@@ -0,0 +1,82 @@
+#include <sys/time.h>
+#include <sstream>
+#include <queue>
+#include <stdio.h>
+#include "rocket/common/log.h"
+#include "rocket/common/util.h"
+
+namespace rocket
+{
+
+    static Logger *g_Logger = new Logger();
+
+    Logger *Logger::GetGlobalLogger()
+    {
+        if (g_Logger)
+        {
+            return g_Logger;
+        }
+        g_Logger = new Logger();
+        return g_Logger;
+    }
+
+    std::string LogLevelToString(LogLevel level)
+    {
+        switch (level)
+        {
+        case Debug:
+            return "DEBUG";
+        case Info:
+            return "INFO";
+        case Error:
+            return "ERROR";
+        default:
+            return "UNKNOWN";
+        }
+    }
+
+    std::string LogEvent::toString()
+    {
+        struct timeval now_time;
+
+        gettimeofday(&now_time, nullptr);
+
+        struct tm now_time_t;
+        localtime_r(&(now_time.tv_sec), &now_time_t);
+
+        char buf[128];
+        strftime(&buf[0], 128, "%y-%m-%d %H:%M:%S", &now_time_t);
+        std::string time_str(buf);
+
+        int ms = now_time.tv_usec / 1000;
+        time_str = time_str + "." + std::to_string(ms);
+
+        m_pid = getPid();
+        m_thread_id = getThreadId();
+
+        std::stringstream ss;
+
+        ss << "[" << LogLevelToString(m_level) << "]\t"
+           << "[" << time_str << "]\t"
+           << "[" << std::string(__FILE__) << ":" << __LINE__ << "]\t";
+
+        return ss.str();
+    }
+
+    void Logger::pushLog(const std::string &msg)
+    {
+        m_buffer.push(msg);
+    }
+
+    void Logger::log()
+    {
+        while (!m_buffer.empty())
+        {
+            std::string msg = m_buffer.front();
+            m_buffer.pop();
+
+            printf(msg.c_str());
+        }
+    }
+
+}
---
+++ rocket/common/log.h
@@ -0,0 +1,84 @@
+#ifndef ROCKET_COMMON_LOG_H
+#define ROCKET_COMMON_LOG_H
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <queue>
+
+namespace rocket
+{
+
+    template <typename... Args>
+    std::string formatString(const char *str, Args &&...args)
+    {
+        int size = snprintf(nullptr, 0, str, args...);
+
+        std::string result;
+        if (size > 0)
+        {
+            result.resize(size);
+            snprintf(&result[0], size + 1, str, args...);
+        }
+        return result;
+    }
+
+#define DUBUGLOG(str, ...)                                                                                                  \
+    std::string msg = new rocket::LogEvent(rocket::LogLevel::Debug)->toString() + rocket::formatString(str, ##__VA_ARGS__); \
+    rocket::Logger::GetGlobalLogger()->pushLog(msg);                                                                        \
+    rocket::Logger::GetGlobalLogger()->log();
+
+    enum LogLevel
+    {
+        Debug = 1,
+        Info = 2,
+        Error = 3
+    };
+
+    class Logger
+    {
+    public:
+        typedef std::shared_ptr<Logger> s_ptr;
+
+        void pushLog(const std::string &msg);
+
+        void log();
+
+    public:
+        static Logger *GetGlobalLogger();
+
+    private:
+        LogLevel m_set_level;
+
+        std::queue<std::string> m_buffer;
+    };
+
+    class LogEvent
+    {
+    public:
+        LogEvent(LogLevel level) : m_level(level) {}
+
+        std::string getFileName() const
+        {
+            return m_file_name;
+        }
+
+        LogLevel getLogLevel() const
+        {
+            return m_level;
+        }
+
+        std::string toString();
+
+    private:
+        std::string m_file_name; // 文件名
+        int32_t m_file_line;
+        int32_t m_pid;
+        int32_t m_thread_id;
+
+        LogLevel m_level;
+    };
+
+}
+
+#endif
