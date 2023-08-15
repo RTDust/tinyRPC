ec3aa402e4452e93420ce1e41a81ac7f4a1fb842
RTDust <chunj@hust.edu.cn>
2023-08-15 03:05:21 -0400

    测试文件提交

--- rocket/testcases/test_log.cc
+++ rocket/testcases/test_log.cc
@@ -1,7 +1,37 @@
+#include <pthread.h>
 #include "rocket/common/log.h"
+#include "rocket/common/config.h"
+
+void *fun(void *)
+{
+
+    int i = 20;
+    while (i--)
+    {
+        DEBUGLOG("debug this is thread in %s", "fun");
+        INFOLOG("info this is thread in %s", "fun");
+    }
+
+    return NULL;
+}
 
 int main()
 {
-    DEBUGLOG("test log %s", "11");
+
+    rocket::Config::SetGlobalConfig(NULL);
+
+    rocket::Logger::InitGlobalLogger(0);
+
+    pthread_t thread;
+    pthread_create(&thread, NULL, &fun, NULL);
+
+    int i = 20;
+    while (i--)
+    {
+        DEBUGLOG("test debug log %s", "11");
+        INFOLOG("test info log %s", "11");
+    }
+
+    pthread_join(thread, NULL);
     return 0;
-}   
+}
